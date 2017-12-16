dea202ea063002350445dcd18ddf6fcbae4ea2d7
Gabriel Forti <gnforti@gmail.com>
2017-12-16 03:35:55 -0500

    Add files via upload

--- trivia2/index.js
+++ trivia2/index.js
@@ -5,10 +5,71 @@ var path = require('path');
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var port = process.env.PORT || 3000;
+var localtunnel = require('localtunnel')
 
-var questions = require('./questions')
+var gameUrl = 'http://localhost:3000';
+
+var es6tunnel =  function (port, options) {
+    return new Promise(function (resolve, reject) {
+    localtunnel(port, options, function (err, tunnel) {
+      return err ? reject(err) : resolve(tunnel)
+    })
+  })
+}
+
+
+var questions = shuffle(require('./questions'))
+
+var curQuestion = 0;
+var totalQuestions = questions.length;
+
+
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+
+
+var nextQuestionDelayMs = 5000; //5secs // how long are players 'warned' next question is coming
+var timeToAnswerMs = 15000; // 15secs // how long players have to answer question
+var timeToEnjoyAnswerMs = 10000; //10secs // how long players have to read answer
+
+var answerData;
+
+/*
+es6tunnel(port).then(tunnel => {
+  // the assigned public url for your tunnel
+  // i.e. https://abcdefgjhij.localtunnel.me
+   gameUrl = tunnel.url;
+   return
+}).catch(err => {
+  if (err) console.log(err)
+  return
+}).then( fin => {
+
+*/
+
+
+
 
 server.listen(port, function () {
+    console.log('Listening on port', gameUrl);
   console.log('Server listening at port %d', port);
 });
 
@@ -18,64 +79,159 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Chatroom
 
 var numUsers = 0;
+var players = {};
+
 
 io.on('connection', function (socket) {
   var addedUser = false;
 
   // when the client emits 'new message', this listens and executes
-  socket.on('new message', function (data) {
+
+  socket.on('host', function () {
     // we tell the client to execute 'new message'
-    socket.broadcast.emit('new message', {
-      username: socket.username,
-      message: data
+
+    totalQuestions = questions.length;
+    curQuestion = totalQuestions + 1
+    numUsers = 0;
+    players = {};
+    socket.emit('host-game-info', {
+      gameUrl: gameUrl
     });
   });
+
 
   // when the client emits 'add user', this listens and executes
   socket.on('add user', function (data) {
-    if (addedUser) return;
-    
-    console.log(data.username, data.icon)
+    if ( players[socket.id]) return;
+
 
     // we store the username in the socket session for this client
-    socket.username = data.username;
-    socket.icon = data.icon;
+
+
+    data.points = 0
+    data.correct = false
+    players[socket.id] = Object.assign({}, data);
+
     ++numUsers;
     addedUser = true;
     socket.emit('login', {
-      numUsers: numUsers
+        userid: socket.id,
+        numUsers: numUsers
     });
     // echo globally (all clients) that a person has connected
     socket.broadcast.emit('user joined', {
-      username: socket.username,
       numUsers: numUsers
     });
   });
 
-  // when the client emits 'typing', we broadcast it to others
-  socket.on('typing', function () {
-    socket.broadcast.emit('typing', {
-      username: socket.username
-    });
-  });
 
-  // when the client emits 'stop typing', we broadcast it to others
-  socket.on('stop typing', function () {
-    socket.broadcast.emit('stop typing', {
-      username: socket.username
+
+    socket.on('start', function () {
+        curQuestion = 0;
+        emitNewQuestion();
     });
-  });
+
+
+
+    socket.on('answer', function (data) {
+        players[socket.id].correct = false
+        if ( answerData === data.answer) {
+            players[socket.id].correct = true
+            players[socket.id].points++
+        }
+
+    });
+
 
   // when the user disconnects.. perform this
   socket.on('disconnect', function () {
-    if (addedUser) {
+    if (players[socket.id]) {
       --numUsers;
 
       // echo globally that this client has left
-      socket.broadcast.emit('user left', {
-        username: socket.username,
+      socket.broadcast.emit('user joined', {
         numUsers: numUsers
       });
+
+      delete players[socket.id]
     }
   });
 });
+
+
+
+//})
+
+function resetPlayerWinners() {
+    Object.keys(players)
+                    .forEach(id => {
+                        players[id].correct = false
+                    })
+}
+
+
+function emitNewQuestion() {
+    resetPlayerWinners()
+
+    io.sockets.emit('question', {
+        totalTime: nextQuestionDelayMs,
+        endTime: new Date().getTime() + nextQuestionDelayMs,
+        choices: [],
+        question:'Next Question ...'
+    });
+
+    setTimeout(function(){
+        if (questions[curQuestion]) {
+            var q = questions[curQuestion];
+            q.endTime = new Date().getTime() + timeToAnswerMs;
+            q.totalTime = timeToAnswerMs;
+
+            answerData = q.answer
+
+            q.choices = shuffle(q.choices)
+            io.sockets.emit('question', q);
+
+            curQuestion++;
+
+            setTimeout(function(){
+                emitAnswer();
+            }, timeToAnswerMs);
+        }
+    }, nextQuestionDelayMs);
+
+}
+
+
+
+function emitAnswer() {
+
+    let data = {}
+    data.correctAnswer = answerData;
+    data.endTime = new Date().getTime() + timeToEnjoyAnswerMs;
+    data.totalTime = timeToEnjoyAnswerMs;
+    data.questionsLeft =  totalQuestions - curQuestion
+
+    io.sockets.emit('correct answer', data); // emit to everyone (no winner)
+
+    io.sockets.emit('answer results', players);
+
+    var leader = Object.keys(players)
+                    .map(key => players[key])
+                    .reduce((prev, current) => (prev.points > current.points) ? prev : current)
+
+    io.sockets.emit('leader', leader);
+
+
+
+    if ( curQuestion < totalQuestions ) {
+       setTimeout(function(){
+           emitNewQuestion();
+       }, timeToEnjoyAnswerMs);
+    } else {
+        var winner = Object.keys(players)
+                    .map(key => players[key])
+                    .reduce((prev, current) => (prev.points > current.points) ? prev : current)
+
+            io.sockets.emit('winner', winner);
+    }
+}
--- trivia2/questions.js
+++ trivia2/questions.js
@@ -1,73 +1,151 @@
 module.exports = [{
-    question: "Which number is NOT a prime number?",
+    question: "What holiday movie sequel includes a cameo by Donald Trump?",
     choices: [
-        '141',
-        '37',
-        '613',
-        '23'
-    ]
+        'Home Alone 2',
+        'Home Alone',
+        'Home Alone 3',
+        'Home Alone 4'
+    ],
+    answer: 'Home Alone 2',
+    img: 'trumphomealone2.png'
 },{
-    question: "What is the smallest number into which 8, 18, and 28 all divide?",
+    question: "In the song 'Frosty the Snowman', what made Frosty come to life?",
     choices: [
-        '504',
-        '252',
-        '28',
-        '2'
-    ]
+        'An old silk hat',
+        'Magic',
+        'Christmas Miracle',
+        'A bright red scarf'
+    ],
+    answer: 'An old silk hat',
+    img: 'frosty.png'
 },{
-    question: "When was Pythagoras alive?",
+    question: "What Christmas beverage is also known as 'milk punch?'",
     choices: [
-        '500 BC',
-        '800 BC',
-        '400 BC',
-        '200 BC'
-    ]
+        'Eggnog',
+        'Hot Cocoa',
+        'Milk punch',
+        'Baby Formula'
+    ],
+    answer: 'Eggnog',
+    img: 'eggnog.png'
 },{
-    question: "Chad's car used 12 gallons of gasoline on a 420 mile trip. How many miles per gallon (mpg) did the car get?",
+    question: "What Christmas-themed ballet premiered in Saint Petersburg, Russia in 1892?",
     choices: [
-        '35 mpg',
-        '25 mpg',
-        '30 mpg',
-        '40 mpg'
-    ]
+        'The Nutcracker',
+        'The Twelve Days of Christmas',
+        'A Christmas Carol',
+        'The Rockettes'
+    ],
+    answer: 'The Nutcracker',
+    img: 'theater.png'
 },{
-    question: "What is the probability of rolling a pair of dice and getting the same number?",
+    question: "What traditional Christmas decoration is actually a parasitic plant?",
     choices: [
-        '1/6',
-        '1/12',
-        '1/4',
-        '1/18'
-    ]
-},{
-    question: "What's the area of a circle with radius 5?",
+        'Mistletoe',
+        'Garland',
+        'Scotch Pine',
+        'Wreath'
+    ],
+    answer: 'Mistletoe',
+    img: 'decoration.png'
+}
+,{
+    question: "How many red ornaments are found on the tree?",
     choices: [
-        '79',
-        '25',
-        '16',
-        '31'
-    ]
-},{
-    question: "What is 15% of 420?",
+        '21',
+        '19',
+        '22',
+        '18'
+    ],
+    answer: '21',
+    img: 'howmanyred.png'
+}
+,{
+    question: "What beverage company has been using Santa Claus in its advertising since 1931?",
     choices: [
-        '63',
-        '57',
-        '61',
-        '69'
-    ]
-},{
-    question: "How much is a googol? Note 10^3 means 10 x 10 x 10",
+        'Coca-Cola',
+        'Pepsi',
+        '7up',
+        'Schweppes'
+    ],
+    answer: 'Coca-Cola',
+    img: 'cola.png'
+}
+,{
+    question: "What well-known Christmas carol became the first song ever broadcast from space in 1965?",
     choices: [
-        '10^100',
-        '10^10',
-        '10^15',
-        '10^20'
-    ]
-},{
-    question: "What is the least prime natural number?",
+        'Jingle Bells',
+        'Hark! The Herald Angels Sing',
+        'We wish you a Merry Christmas',
+        'Oh Holy Night'
+    ],
+    answer: 'Jingle Bells',
+    img: 'recordplayer.png'
+}
+,{
+    question: "What kind of Christmas does Elvis Presley sing about?",
     choices: [
-        '2',
-        '1',
-        '3',
-        '0'
-    ]
-}];
+        'Blue',
+        'White',
+        'Red',
+        'Snowy'
+    ],
+    answer: 'Blue',
+    img: 'elvis.png'
+}
+,{
+    question: "Which Christmas movie has been played more than any other?",
+    choices: [
+        "It's a Wonderful Life",
+        'A Christmas Story',
+        'Home Alone',
+        'Frosty the Snowman'
+    ],
+    answer: "It's a Wonderful Life",
+    img: 'wonderfullife.png'
+}
+,{
+    question: "The Christmas story is in which book(s) of the bible?",
+    choices: [
+        "Matthew and Luke",
+        'All four of the Gospels',
+        'Only Luke',
+        'Only Matthew'
+    ],
+    answer: "Matthew and Luke",
+    img: 'manger.png'
+}
+,{
+    question: "What animated 2004 film is about a train that carries kids to the North Pole on Christmas Eve?",
+    choices: [
+        "The Polar Express",
+        'Rudolph the Red-Nosed Reindeer',
+        'Elf',
+        'The Santa Clause'
+    ],
+    answer: "The Polar Express",
+    img: 'thepolarexpress.png'
+}
+,{
+    question: "What film is about a father who vows to get his son a Turbo Man action figure for Christmas?",
+    choices: [
+        "Jingle All the Way",
+        'Miracle on 34th Street',
+        'A Christmas Story',
+        'A Christmas Carol'
+    ],
+    answer: "Jingle All the Way",
+    img: 'jinglealltheway.png'
+}
+,{
+    question: "In Charles Dickens' A Christmas Carol, what was Mr. Scrooge's first name?",
+    choices: [
+        "Ebenezer",
+        'Gabriel',
+        'Charles',
+        'Hansel'
+    ],
+    answer: "Ebenezer",
+    img: 'tinytim.png'
+}
+];
